3ed8d0cc52545fd4dd3b65fa19d02ef08df7786b
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-02-20 22:10:10 +0200

    add validation for password strength

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -2,7 +2,7 @@ from datetime import datetime, timezone
 from typing import cast
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy import select, delete
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session, joinedload
 from config import get_jwt_auth_manager, get_settings, BaseAppSettings
@@ -28,15 +28,20 @@ async def register_user(user_data: UserRegistrationRequestSchema, db: AsyncSessi
     group = result.scalar_one_or_none()
     if not group:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Default group not found")
-    user = UserModel.create(email=user_data.email, raw_password=user_data.password, group_id=group.id)
-    db.add(user)
     try:
-        await db.flush()
-    except IntegrityError:
+        user = UserModel.create(email=user_data.email, raw_password=user_data.password, group_id=group.id)
+        db.add(user)
+        try:
+            await db.flush()
+        except IntegrityError:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail=f"A user with this email {user.email} already exists."
+            )
+        activation_token = ActivationTokenModel(user_id=user.id)
+        db.add(activation_token)
+        await db.commit()
+        return user
+    except SQLAlchemyError:
         raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT, detail=f"A user with this email {user.email} already exists."
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="An error occurred during user creation."
         )
-    activation_token = ActivationTokenModel(user_id=user.id)
-    db.add(activation_token)
-    await db.commit()
-    return user

e41cf37b2afc2bb61fdb4daab35cebb7b9756e67
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-02-20 19:45:33 +0200

    add schemas

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -1,6 +1,47 @@
-from pydantic import BaseModel, EmailStr, field_validator
+from pydantic import BaseModel, EmailStr, field_validator, ConfigDict
 
 from database import accounts_validators
 
 
-# Write your code here
+class UserRegistrationRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserRegistrationResponseSchema(BaseModel):
+    id: int
+    email: EmailStr
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserActivationRequestSchema(BaseModel):
+    pass
+
+
+class MessageResponseSchema(BaseModel):
+    pass
+
+
+class PasswordResetRequestSchema(BaseModel):
+    pass
+
+
+class PasswordResetCompleteRequestSchema(BaseModel):
+    pass
+
+
+class UserLoginResponseSchema(BaseModel):
+    pass
+
+
+class UserLoginRequestSchema(BaseModel):
+    pass
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    pass
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    pass

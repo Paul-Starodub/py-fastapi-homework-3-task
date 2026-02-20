bd2349ad9f03a98dbc65b427261df6d2adb2566a
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-02-20 20:54:48 +0200

    add route for user creation

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -1,12 +1,10 @@
 from datetime import datetime, timezone
 from typing import cast
-
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy import select, delete
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session, joinedload
-
 from config import get_jwt_auth_manager, get_settings, BaseAppSettings
 from database import (
     get_db,
@@ -15,11 +13,30 @@ from database import (
     UserGroupEnum,
     ActivationTokenModel,
     PasswordResetTokenModel,
-    RefreshTokenModel
+    RefreshTokenModel,
 )
 from exceptions import BaseSecurityError
+from schemas.accounts import UserRegistrationRequestSchema, UserRegistrationResponseSchema
 from security.interfaces import JWTAuthManagerInterface
 
 router = APIRouter()
 
-# Write your code here
+
+@router.post("/register/", response_model=UserRegistrationResponseSchema, status_code=status.HTTP_201_CREATED)
+async def register_user(user_data: UserRegistrationRequestSchema, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(UserGroupModel).where(UserGroupModel.name == UserGroupEnum.USER))
+    group = result.scalar_one_or_none()
+    if not group:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Default group not found")
+    user = UserModel.create(email=user_data.email, raw_password=user_data.password, group_id=group.id)
+    db.add(user)
+    try:
+        await db.flush()
+    except IntegrityError:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail=f"A user with this email {user.email} already exists."
+        )
+    activation_token = ActivationTokenModel(user_id=user.id)
+    db.add(activation_token)
+    await db.commit()
+    return user

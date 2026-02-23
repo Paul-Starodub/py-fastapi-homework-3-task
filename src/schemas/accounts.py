0014df9d51125b8b45f0dd3acf8082e80970c007
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-02-23 18:04:20 +0200

    update schemas

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -50,8 +50,8 @@ class UserLoginRequestSchema(BaseModel):
 
 
 class TokenRefreshRequestSchema(BaseModel):
-    pass
+    refresh_token: str
 
 
 class TokenRefreshResponseSchema(BaseModel):
-    pass
+    access_token: str
